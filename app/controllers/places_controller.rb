dc299295dc76562e6d29eb66aab92dd939f6c087
Alisonhardison <appletini19@gmail.com>
2019-08-19 14:42:07 +0000

    validation error

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -11,9 +11,12 @@ class PlacesController < ApplicationController
   end
 
   def create
-    Place.create(place_params)
-    current_user.places.create(place_params)
+    @place = current_user.places.create(place_params)
+    if @place.valid?
     redirect_to root_path
+    else
+      render :new, status: :unprocessable_enity
+    end
   end
 
   def show
@@ -31,8 +34,16 @@ class PlacesController < ApplicationController
 
   def update
     @place = Place.find(params[:id])
+    if @place.user != current_user
+      return render plain: 'Not Allowed', status: :forbidden
+    end
+
     @place.update_attributes(place_params)
-    redirect_to root_path
+    if @place.valid?
+      redirect_to root_path
+    else
+      render :edit, status: :unprocessable_enity
+    end
   end
 
   def destroy
