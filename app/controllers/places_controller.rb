f802667723a5de4606a58a65d992b77e290c15fb
Alisonhardison <appletini19@gmail.com>
2019-08-19 13:29:34 +0000

    user edit only

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -22,8 +22,13 @@ class PlacesController < ApplicationController
 
   def edit
     @place = Place.find(params[:id])
+    
+    if @place.user != current_user
+      return render plain: 'Not Allowed', status: :forbidden
+    end
   end
  
+
   def update
     @place = Place.find(params[:id])
     @place.update_attributes(place_params)
