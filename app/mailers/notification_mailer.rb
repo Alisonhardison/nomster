59e735e385d7911664b4b1507e4b0caa32984901
Alisonhardison <appletini19@gmail.com>
2019-08-29 13:34:03 +0000

    email preview

--- app/mailers/notification_mailer.rb
+++ app/mailers/notification_mailer.rb
@@ -2,8 +2,8 @@ class NotificationMailer < ApplicationMailer
   default from: "no-reply@nomsterapp.com"
 
   def comment_added
-    mail(to: "appletini19@gmail.com",
-          subject: "A comment has been added to your place")
+    mail(to: "pattonalison2@gmail.com",
+         subject: "A comment has been added to your place")
   end
 
 end
